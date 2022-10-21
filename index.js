e7b2aed9ea90eebc99686e88aeaee41d717dea53
HannaK-Git <annakovally@gmail.com>
2022-10-21 11:16:52 +0300

    env for mongo was added

--- index.js
+++ index.js
@@ -29,13 +29,7 @@ app.use("/api", router);
 app.use(errorHandler);
 
 
-// For deployment
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+
 
 
 const start = async () => {
